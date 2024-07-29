953524f19c2b0be7756f5d15f866710b451e4653
Ravi Rai <Ravi.B.Rai@outlook.com>
2024-07-28 23:10:58 -0300

    started chat to docs project

---
+++ app.py
@@ -0,0 +1,27 @@
+import streamlit as st
+from dotenv import load_dotenv
+from PyPDF2 import PdfReader
+from langchain.text_splitter import CharacterTextSplitter
+from langchain.embeddings import OpenAIEmbeddings, HuggingFaceInstructEmbeddings
+from langchain.vectorstores import FAISS
+from langchain.chat_models import ChatOpenAI
+from langchain.memory import ConversationBufferMemory
+from langchain.chains import ConversationalRetrievalChain
+#from htmltemplate import css, bot_template, user_template
+from langchain.llms import HuggingFaceHub
+from langchain.llms import OpenAI
+from langchain.chains.summarize import load_summarize_chain
+
+
+def main():
+    st.set_page_config(page_title="Chat with multiple PDFs", page_icon=":books:")
+
+    st.header("Chat with multiple PDFs :books:")
+    st.text_input("Ask a question about your documents:")
+
+    with st.sidebar:
+        st.subheader("Your documents")
+
+
+if __name__ == '__main__':
+    main()
